13921bad4214ec9c5cc76da40c1a39397ffd1d55
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2023-05-20 20:24:26 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-class Config(BaseSettings):
-    livecheck_hour: int = 1
-    livecheck_url: str
-    class Config:
-        extra = "ignore"
-        case_sensitive = False
